ebb9f2ab73419daaf0aa4bae86f09af24966e41c
ElijahJack <League@AdministorsiMac.attlocal.net>
2018-02-21 14:59:20 -0800

    Day 2, was really fun and interesting, we worked on Hello World and much more

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,8 +6,17 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+Robot rob = new Robot();
 	void go() {
+		rob.setSpeed(10000);
+		rob.move(100);
+		rob.penDown();
+		for(int i =0; i<5;i++) {
+		rob.setRandomPenColor();
+		drawSquare();
+		rob.turn(90);}
+	
+		
 		// 4. Make the robot move as fast as possible
 
 		// 5. Set the pen width to 5
@@ -25,6 +34,14 @@ public class FourSquare {
 	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+		rob.move(100);
+		rob.turn(90);
+		rob.move(100);
+		rob.turn(90);
+		rob.move(100);
+		rob.turn(90);
+		rob.move(100);
+		rob.turn(90);
 
 		
 		
